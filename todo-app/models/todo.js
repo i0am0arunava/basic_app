9876ccedb80595765576061761792d0ba097d913
arunava369369 <arunavapari536@gmail.com>
2023-09-29 00:27:20 +0530

    first commit

--- todo-app/models/todo.js
+++ todo-app/models/todo.js
@@ -8,38 +8,67 @@ module.exports = (sequelize, DataTypes) => {
   class Todo extends Model {
     /**
      * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
+     * this method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
       // define association here
     }
-   
-    static addTodo({title,duedate}){
-      return this.create({title: title,duedate:duedate,markAsComplete:false})
+
+    static addTodo({ title, duedate }) {
+      return Todo.create({ title: title, duedate: duedate, markAsComplete: false })
     }
- 
-   
+
     static async overdue() {
+      return await Todo.findAll({
+        where: {
+          duedate: {
+            [Op.lt]: new Date().toLocaleDateString("en-CA"),
+          },
+          markAsComplete: false
+        },
+        order: [["id", "ASC"]],
+      });
+    }
+    static async dueLater() {
       // FILL IN HERE TO RETURN OVERDUE ITEMS
-   
-       return await  Todo.findAll({
-        
-       });
+
+      return await Todo.findAll({
+        where: {
+          duedate: {
+            [Op.gt]: new Date(),
+          },
+          markAsComplete: false
         }
-        static async duelater() {
-          // FILL IN HERE TO RETURN OVERDUE ITEMS
-       
-           return await  Todo.findAll({
-              where:{
-                duedate:{
-                [Op.gt] : new Date(),
-                },
-           }
-           });
-            }
-    markAsCompleted (){
-      return this.update({ markAsComplete:true})
+      });
+    }
+    static dueToday() {
+      return this.findAll({
+        where: {
+          duedate: {
+            [Op.eq]: new Date().toLocaleDateString("en-CA"),
+          },
+          markAsComplete: false
+
+        },
+        order: [["id", "ASC"]],
+      });
+    }
+    static async completedItems() {
+      return await Todo.findAll({
+        where: {
+          markAsComplete: true,
+        },
+        order: [["id", "ASC"]],
+      });
+    }
+    markAsCompleted() {
+      if (this.markAsComplete == true) {
+        return this.update({ markAsComplete: false })
+      }
+      else if (this.markAsComplete == false) {
+        return this.update({ markAsComplete: true })
+      }
     }
   }
   Todo.init({
